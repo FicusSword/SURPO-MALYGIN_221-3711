b971a628309f8206f6f5dce62d2745d2c0febb49
FicusSword <Sasha.Malygin1994@gmail.com>
2023-11-07 02:08:30 +0300

    фывф
    фывв

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Person
+{
+    public string name = "Ben";
+    public int age = 18;
+    public string email = "ben@gmail.com";
+    public Person(string name)
+    {
+        this.name = name;
+    }
+    public Person(string name, int age) : this(name)
+    {
+        this.age = age;
+    }
+    public Person(string name, int age, string email) : this(name, age)
+    {
+        this.email = email;
+    }
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {name}, Age: {age}, Email: {email}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Person person1 = new Person("Leon");
+        Person person2 = new Person("Ada", 25);
+        Person person3 = new Person("Kris", 30, "Kris@gmail.com");
+        person1.DisplayInfo();
+        person2.DisplayInfo();
+        person3.DisplayInfo();
+    }
+}
